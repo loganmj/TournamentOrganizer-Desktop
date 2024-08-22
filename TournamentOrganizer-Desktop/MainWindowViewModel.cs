f8f74975f4da6dac3e766baab5edaa2ad215a3ef
Michael <michaellogan7860@gmail.com>
2024-08-22 14:32:14 -0500

    TOD-19 Added function to Create button.

--- TournamentOrganizer-Desktop/MainWindowViewModel.cs
+++ TournamentOrganizer-Desktop/MainWindowViewModel.cs
@@ -1,6 +1,9 @@
 using TournamentOrganizer_Desktop.ViewModels;
 using TournamentOrganizer_Desktop.Models;
 using CommunityToolkit.Mvvm.ComponentModel;
+using System.Windows.Input;
+using CommunityToolkit.Mvvm.Input;
+using System.Windows;
 
 namespace TournamentOrganizer_Desktop;
 
@@ -28,6 +31,21 @@ public partial class MainWindowViewModel : ViewModelBase
     {
         Title = "Tournament Organizer";
         _tournamentNameInput = "New Tournament";
+    }
+
+    #endregion
+
+    #region Private Methods
+
+    /// <summary>
+    /// Handles the Create button being clicked.
+    /// </summary>
+    [RelayCommand]
+    private void OnCreateButtonClick()
+    {
+        Tournament = new SwissTournament(TournamentNameInput);
+
+        MessageBox.Show($"Created new tournament with ID={Tournament.ID}, Name={Tournament.Name}");
     }
 
     #endregion
