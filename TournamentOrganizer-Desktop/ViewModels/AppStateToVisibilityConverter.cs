b7e3ff93c670b20377773c0513367fa8b6f54cd7
Michael <michaellogan7860@gmail.com>
2024-08-29 14:52:36 -0500

    TOD-28 Added a booleaninverter and added it to the pairing UI to fix the counter enable inversion.

--- TournamentOrganizer-Desktop/ViewModels/AppStateToVisibilityConverter.cs
+++ TournamentOrganizer-Desktop/ViewModels/AppStateToVisibilityConverter.cs
@@ -2,45 +2,44 @@
 using System.Windows;
 using System.Windows.Data;
 
-namespace TournamentOrganizer_Desktop.ViewModels
+namespace TournamentOrganizer_Desktop.ViewModels;
+
+/// <summary>
+/// Returns a visibility value based on the passed in app state, and a determining match value.
+/// </summary>
+public class AppStateToVisibilityConverter : IValueConverter
 {
     /// <summary>
-    /// Returns a visibility value based on the passed in app state, and a determining match value.
+    /// Compares the app state to the passed in value.
+    /// If the values match, the component is visible. 
+    /// Otherwise the component is collapsed.
     /// </summary>
-    public class AppStateToVisibilityConverter : IValueConverter
+    /// <param name="value"></param>
+    /// <param name="targetType"></param>
+    /// <param name="parameter"></param>
+    /// <param name="culture"></param>
+    /// <returns>A Visibility value.</returns>
+    public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
     {
-        /// <summary>
-        /// Compares the app state to the passed in value.
-        /// If the values match, the component is visible. 
-        /// Otherwise the component is collapsed.
-        /// </summary>
-        /// <param name="value"></param>
-        /// <param name="targetType"></param>
-        /// <param name="parameter"></param>
-        /// <param name="culture"></param>
-        /// <returns>A Visibility value.</returns>
-        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        if (value is AppState appState && parameter is AppState targetState)
         {
-            if (value is AppState appState && parameter is AppState targetState)
-            {
-                return appState == targetState ? Visibility.Visible : Visibility.Collapsed;
-            }
-
-            return Visibility.Collapsed;
+            return appState == targetState ? Visibility.Visible : Visibility.Collapsed;
         }
 
-        /// <summary>
-        /// Not used.
-        /// </summary>
-        /// <param name="value"></param>
-        /// <param name="targetType"></param>
-        /// <param name="parameter"></param>
-        /// <param name="culture"></param>
-        /// <returns></returns>
-        /// <exception cref="NotImplementedException"></exception>
-        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
-        {
-            throw new NotImplementedException();
-        }
+        return Visibility.Collapsed;
+    }
+
+    /// <summary>
+    /// Not used.
+    /// </summary>
+    /// <param name="value"></param>
+    /// <param name="targetType"></param>
+    /// <param name="parameter"></param>
+    /// <param name="culture"></param>
+    /// <returns></returns>
+    /// <exception cref="NotImplementedException"></exception>
+    public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+    {
+        throw new NotImplementedException();
     }
 }
